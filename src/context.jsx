7e7800619b66e7924effb72bfcc7ffc462acd843
Warren <warrendsteinacker@gmail.com>
2025-12-13 12:51:30 -0500

    createing fetch commnand

--- src/context.jsx
+++ src/context.jsx
@@ -1,6 +1,17 @@
 import { createContext, useEffect, useState } from "react";
 import Data from "./componets/data";
 
+
+async function fetchData() {
+    const response = await fetch('http://localhost:3000/api/hello');
+    if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const result = await response.json();
+    return result;
+}
+
+
 DataContext = createContext({
     data: [],
     setData: () => {},
