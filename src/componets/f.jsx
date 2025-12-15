cccd0e180f1108d3d742a68d6e09aec66b2883b0
Warren <warrendsteinacker@gmail.com>
2025-12-15 13:33:46 -0500

    got the structer

--- src/componets/f.jsx
+++ src/componets/f.jsx
@@ -1,10 +1,39 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
+import {useParams} from "react-router-dom"
+import { DataContext } from "../context";
+
+
+
+
+
 
 
 export const Editpage_for_post = () => {
+    
+
+    const {data, upFunc} = useContext 
+    
+    const {id} = useParams()
+
+    const post = data.filter((item) => item.id === id)
+
+
+    return(
+    <>  
+    
+    
+    
+    <form onSubmit={}>
+    <input value={post.name} type="text"/>
+    <input value={post.count} type="text"/>
+    <input value={post.description} type="text"/>
+    <button type="submit">Update Item</button>
+    </form>
 
 
 
+
+    </>)
 
 }
 
