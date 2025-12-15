7a611849b31a031227428d6020869e4d237bc803
Warren <warrendsteinacker@gmail.com>
2025-12-15 14:10:44 -0500

    made edit page

--- src/componets/f.jsx
+++ src/componets/f.jsx
@@ -11,22 +11,51 @@ import { DataContext } from "../context";
 export const Editpage_for_post = () => {
     
 
-    const {data, upFunc} = useContext 
+    const {data, upFunc} = useContext(DataContext) 
     
     const {id} = useParams()
 
     const post = data.filter((item) => item.id === id)
 
 
+    const [postdescript, setDescrip] = useState([post.description])
+
+    const [postcount, setCount] = useState([post.count])
+
+    const [postname, setName] = useState([post.name])
+
+    const [up2date, setNpost] = useState([])
+
+    const f = (e) => {
+        setCount(e.target.value)
+    }
+
+    const ff = (e) => {
+        setName(e.target.value)
+    }
+
+    const fff = (e) => {
+        setDescrip(e.target.value)
+    }
+
+    const up2datefunc = () => {
+        setNpost({count: postcount,
+            name: postname,
+            description: postdescript,
+        })
+
+        upFunc(up2date)
+
+    }
     return(
     <>  
     
     
     
-    <form onSubmit={}>
-    <input value={post.name} type="text"/>
-    <input value={post.count} type="text"/>
-    <input value={post.description} type="text"/>
+    <form onSubmit={up2datefunc}>
+    <input value={post.name} onChange={f()} type="text"/>
+    <input value={post.count} onChange={ff()} type="text"/>
+    <input value={post.description} onChange={fff()} type="text"/>
     <button type="submit">Update Item</button>
     </form>
 
