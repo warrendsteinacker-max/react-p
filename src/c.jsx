0057f7010d5be1ce41ffc63412b68ffde05eb1a8
Warren <warrendsteinacker@gmail.com>
2025-12-15 14:32:04 -0500

    creating home page

--- src/c.jsx
+++ src/c.jsx
@@ -1,3 +1,7 @@
+import {Link} from 'react-router-dom'
+
+
+
 
 export const Home = () => {
 
@@ -5,26 +9,22 @@ const {data} = useContext(DataContext);
 
     return (
         <>
+                <link href="/postdata"> add to storage</link>
+                <link href="/search"> filter storage</link>
                     {data ? (
                         data.map((item, index) => (
                             <div key={index}>
-                                <h2>{item.title}</h2>
+                                <h2>{item.name}</h2>
                                 <p>{item.description}</p>
                                 <p>{item.count}</p>
                                 
-                                <button onClick={}>increase item count</button>
-                                <button onClick={}>decrease item count</button>
-
+                            <>delete ${item.name}</>
                             </div>
                         ))
                     ) : (
                         // Show a loading state while fetching
-                        <div>Loading data...</div>
+                        <div>There are no items</div>
                     )}
-
-<link href="/postdata"> add to storage</link>
-<link href="/filterdata"> filter storage</link>
-<link href="/deletedata"> delete storage</link>
         </>
 
 
