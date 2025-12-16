13a3687a3ebfd706b080ac18a5ea8d653c8fe266
Warren <warrendsteinacker@gmail.com>
2025-12-16 16:37:34 -0500

    practicing search techniqs

--- src/practice.jsx
+++ src/practice.jsx
@@ -1,1 +1,15 @@
 // practice 
+
+const p = ({search, setSearch}) => {
+const searching = (e) => {
+    setSearch(e.target.value)
+}
+    <input value={search} onChange={(e)=> searching(e)} />
+} 
+
+
+<Home items={items.toLowercase.includes(search)}/>
+
+const Home = ({}) => {
+    {}
+}
